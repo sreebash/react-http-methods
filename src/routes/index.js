dd7ced2c985b622258d12bfb75acaf2dc807cade
Sreebash kumar <sreebashdas.1@gmail.com>
2022-08-20 22:11:00 +0600

    add complete protected route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "../pages/Home";
 import AddBlog from "../pages/AddBlog";
 import About from "../pages/About";
 import Error from "../pages/Error";
 import Navbar from "../layout/Navbar";
+import Protected from "./Protected";
 
 const Index = () => {
+    
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    
     return (
         <BrowserRouter>
             <Navbar/>
+            {isLoggedIn ? <button onClick={() => {
+                setIsLoggedIn(!isLoggedIn)
+            }}> Log out</button> : <button onClick={() => {
+                setIsLoggedIn(!isLoggedIn)
+            }}>Log In</button>}
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/add-blog" element={<AddBlog/>}/>
+                <Route path="/add-blog" element={<Protected isLoggedIn={isLoggedIn}>
+                    <AddBlog/>
+                </Protected>}/>
                 <Route path="/about" element={<About/>}/>
                 <Route path="/*" element={<Error/>}/>
-                
+            
             </Routes>
         </BrowserRouter>
     
