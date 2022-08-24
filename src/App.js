b470924272f0751a4b70e6dc6e0b4ffc997f914f
Sreebash kumar <sreebashdas.1@gmail.com>
2022-08-24 14:50:04 +0600

    Fetch and display users and design btn

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
-import React, { Component } from 'react';
-import Index from "./routes";
+import React, {useEffect, useState} from 'react';
 import "./App.css";
 
+const url = "https://rest-api-without-db.herokuapp.com/users"
 
-class App extends Component {
-  render() {
+
+const App = () => {
+    
+    const [users, setUsers] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    
+    
+    const getAllUsers = () => {
+        fetch(url)
+            .then((res) => {
+                if (!res.ok) {
+                    throw Error("could not fetch")
+                }
+                return res.json()
+            })
+            .then((data) => {
+                console.log(data.users)
+                setUsers(data.users);
+            })
+            .catch((err) => {
+                setError(err.message)
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+        
+    }
+    
+    useEffect(() => {
+        getAllUsers();
+    }, [])
     return (
-      <div>
-          <Index/>
-      </div>
+        <div className="App">
+            <h1>User Management App</h1>
+            {isLoading && <h2>Loading...</h2>}
+            {error && <h2>{error}</h2>}
+            
+            {users && users.map((user) => {
+                const {id, username, email} = user;
+                return (
+                    <article key={id} className="card">
+                        <p>{username}</p>
+                        <p>{email}</p>
+                        <button className="btn">Add</button>
+                        <button className="btn">Delete</button>
+                    </article>
+                )
+            })}
+        
+        </div>
     );
-  }
-}
+};
 
 export default App;
